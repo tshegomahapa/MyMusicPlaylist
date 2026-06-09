5e4fbf961353e29e45982388c54fda37fed9f22d
tshegomahapa <tshegofatsomahapa19@gmail.com>
2026-06-09 13:41:07 +0200

    commit

--- app/src/main/java/com/example/mymusicplaylist/AddToPlaylist.kt
+++ app/src/main/java/com/example/mymusicplaylist/AddToPlaylist.kt
@@ -1,14 +1,24 @@
 package com.example.mymusicplaylist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymusicplaylist.ui.theme.MyMusicPlaylistTheme
@@ -19,29 +29,49 @@ class AddToPlaylist : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyMusicPlaylistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column() {
+                    Text("Enter song details")
+                    var songTitle by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = songTitle,
+                        onValueChange = { songTitle = it },
+                        label = { Text("Song Title") },
+                        placeholder = { Text("Song Title") }
                     )
+                    var artistName by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = artistName,
+                        onValueChange = { artistName = it },
+                        label = { Text("Artist Name") },
+                        placeholder = { Text("Artist Name") }
+                    )
+                    var rating by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = rating,
+                        onValueChange = { rating = it },
+                        label = { Text("Rating(1-5)") },
+                        placeholder = { Text("Rating(1-5)") }
+                    )
+                    var comments by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = comments,
+                        onValueChange = { comments = it },
+                        label = { Text("Comments") },
+                        placeholder = { Text("Comments") }
+                    )
+                    Row() {
+                        Button(
+                            onClick = {
+                                val intent = Intent(this@AddToPlaylist, MainActivity::class.java)
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text("Home")
+
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyMusicPlaylistTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/mymusicplaylist/MainActivity.kt
+++ app/src/main/java/com/example/mymusicplaylist/MainActivity.kt
@@ -10,9 +10,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mymusicplaylist.ui.theme.MyMusicPlaylistTheme
@@ -35,12 +40,15 @@ class MainActivity : ComponentActivity() {
                     }
                     Button(
                         onClick = {
+                            val intent = Intent(this@MainActivity, DetailedView::class.java)
+                            startActivity(intent)
                         }
                     ) {
                         Text("Detailed View")
                     }
                     Button(
                         onClick = {
+                            finishAffinity()
                         }
                     ) {
                         Text("Exit")
